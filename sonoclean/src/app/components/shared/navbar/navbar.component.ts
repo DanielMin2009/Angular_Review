98eeeef525967dc74805ec3d11bde77169eec85f
DanielMin2009 <daniel.minguella.blasco@gmail.com>
2020-07-20 18:29:40 +0200

    setting navbar

--- sonoclean/src/app/components/shared/navbar/navbar.component.ts
+++ sonoclean/src/app/components/shared/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  HostListener,
+  ViewChildren,
+  QueryList,
+} from '@angular/core';
+import { Router } from '@angular/router';
+// import * as $ from "jquery";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
